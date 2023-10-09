2d9660ab305816dd8413f587dce60212307b63b8
Harsh Singhal <harshparkar27@gmail.com>
2023-10-09 12:54:21 +0530

    dropdown items added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import Home from './components/home';
 import Navbar from './components/navbar';
 import About from './components/about';
 import Devices from './components/devices';
+import Gateway from './components/gateway';
+import Kit4g5g from './components/4g-5g';
+import Camera from './components/camera';
+import Development from './components/development';
+import Node from './components/node';
+import WifiBoard from './components/wifi-board';
+import Monitor from './components/monitor';
+import Weather from './components/weather';
+import Module from './components/module';
+import Shield from './components/shield';
+import Battery from './components/battery';
+import Sht40 from './components/sht40';
+import Stts751 from './components/stts751';
+import Lis3dh from './components/lis3dh';
+import Relay from './components/relay';
+import Lcd from './components/lcd';
+import Buzzer from './components/buzzer';
+import Memory from './components/memory';
+import Lux from './components/lux';
+import Uart from './components/uart';
+
+
 
 function App() {
   return (
@@ -15,6 +37,26 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/devices" element={<Devices/>} />
+          <Route path="/gateway" element={<Gateway/>} />
+          <Route path="/developmemt" element={<Development/>} />
+          <Route path="/node" element={<Node/>} />
+          <Route path="/camera" element={<Camera/>} />
+          <Route path="/4g-5g" element={<Kit4g5g/>} />
+          <Route path="/wifi" element={<WifiBoard/>} />
+          <Route path="/monitor" element={<Monitor/>} />
+          <Route path="/weather" element={<Weather/>} />
+          <Route path="/module" element={<Module/>} />
+          <Route path="/shield" element={<Shield/>} />
+          <Route path="/battery" element={<Battery/>} />
+          <Route path="/sht40" element={<Sht40/>} />
+          <Route path="/stts751" element={<Stts751/>} />
+          <Route path="/lis3dh" element={<Lis3dh/>} />
+          <Route path="/relay" element={<Relay/>} />
+          <Route path="/lcd" element={<Lcd/>} />
+          <Route path="/buzzer" element={<Buzzer/>} />
+          <Route path="/memory" element={<Memory/>} />
+          <Route path="/lux" element={<Lux/>} />
+          <Route path="/uart" element={<Uart/>} />
         </Routes>
     </Router>
     </>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import logo from '../images/logo.png';
+import '../css/home.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <img class="event-image" src={logo} alt="iHub-AWaDH logo" />
+                <a className="navbar-brand" href="/">AWaDH CPS Lab</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About Us</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Items in CPS Lab
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="/gateway">BLE Gateway</a></li>
+                                <li><a className="dropdown-item" href="/camera">Low Power Camera Module</a></li>
+                                <li><a className="dropdown-item" href="/development">BLE Development Kit</a></li>
+                                <li><a className="dropdown-item" href="/node">BLE Node</a></li>
+                                <li><a className="dropdown-item" href="/4g-5g-kit">4G/5G Kit</a></li>
+                                <li><a className="dropdown-item" href="/wifi-board">Wifi Development Board</a></li>
+                                <li><a className="dropdown-item" href="/monitor">Activity Monitor Kit</a></li>
+                                <li><a className="dropdown-item" href="/weather">Weather Monitoring Sensor System</a></li>
+                                <li><a className="dropdown-item" href="/module">Solar Charging & Discharging Module</a></li>
+                                <li><a className="dropdown-item" href="/shield">Grove Shield</a></li>
+                                <li><a className="dropdown-item" href="/battery">Solar Panel and Battery</a></li>
+                                <li><a className="dropdown-item" href="/sht40">SHT40 Sensor</a></li>
+                                <li><a className="dropdown-item" href="/stts751">STTS751 Sensor</a></li>
+                                <li><a className="dropdown-item" href="/lis3dh">LIS3DH Sensor</a></li>
+                                <li><a className="dropdown-item" href="/relay">RELAY Sensor</a></li>
+                                <li><a className="dropdown-item" href="/lcd">LCD</a></li>
+                                <li><a className="dropdown-item" href="/buzzer">Buzzer</a></li>
+                                <li><a className="dropdown-item" href="/memory">Memory Unit (W25Q16)</a></li>
+                                <li><a className="dropdown-item" href="/lux">Lux Sensor</a></li>
+                                <li><a className="dropdown-item" href="/uart">UART to USB Converter</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
