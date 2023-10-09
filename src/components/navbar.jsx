3664864910e8ac317a1edc78e8b6bbed690f50e2
Harsh Singhal <harshparkar27@gmail.com>
2023-10-09 19:11:28 +0530

    revert changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,43 +7,43 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <img src={logo} alt="iHub-AWaDH logo" />
-                <link className="navbar-brand" to="/">AWaDH CPS Lab</link>
+                <a className="navbar-brand" href="/">AWaDH CPS Lab</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <link className="nav-link active" aria-current="page" to="/">Home</link>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <link className="nav-link active" to="/about">About Us</link>
+                            <a className="nav-link active" href="/about">About Us</a>
                         </li>
                         <li className="nav-item dropdown">
-                            <link className="nav-link active dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a className="nav-link active dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Items in CPS Lab
-                            </link>
+                            </a>
                             <ul className="dropdown-menu">
-                                <li><link className="dropdown-item" to="/gateway">BLE Gateway</link></li>
-                                <li><link className="dropdown-item" to="/camera">Low Power Camera Module</link></li>
-                                <li><link className="dropdown-item" to="/development">BLE Development Kit</link></li>
-                                <li><link className="dropdown-item" to="/node">BLE Node</link></li>
-                                <li><link className="dropdown-item" to="/4g-5g-kit">4G/5G Kit</link></li>
-                                <li><link className="dropdown-item" to="/wifi-board">Wifi Development Board</link></li>
-                                <li><link className="dropdown-item" to="/monitor">Activity Monitor Kit</link></li>
-                                <li><link className="dropdown-item" to="/weather">Weather Monitoring Sensor System</link></li>
-                                <li><link className="dropdown-item" to="/module">Solar Charging & Discharging Module</link></li>
-                                <li><link className="dropdown-item" to="/shield">Grove Shield</link></li>
-                                <li><link className="dropdown-item" to="/battery">Solar Panel and Battery</link></li>
-                                <li><link className="dropdown-item" to="/sht40">SHT40 Sensor</link></li>
-                                <li><link className="dropdown-item" to="/stts751">STTS751 Sensor</link></li>
-                                <li><link className="dropdown-item" to="/lis3dh">LIS3DH Sensor</link></li>
-                                <li><link className="dropdown-item" to="/relay">RELAY Sensor</link></li>
-                                <li><link className="dropdown-item" to="/lcd">LCD</link></li>
-                                <li><link className="dropdown-item" to="/buzzer">Buzzer</link></li>
-                                <li><link className="dropdown-item" to="/memory">Memory Unit (W25Q16)</link></li>
-                                <li><link className="dropdown-item" to="/lux">Lux Sensor</link></li>
-                                <li><link className="dropdown-item" to="/uart">UART to USB Converter</link></li>
+                            <li><a className="dropdown-item" href="/gateway">BLE Gateway</a></li>
+                                <li><a className="dropdown-item" href="/camera">Low Power Camera Module</a></li>
+                                <li><a className="dropdown-item" href="/development">BLE Development Kit</a></li>
+                                <li><a className="dropdown-item" href="/node">BLE Node</a></li>
+                                <li><a className="dropdown-item" href="/4g-5g-kit">4G/5G Kit</a></li>
+                                <li><a className="dropdown-item" href="/wifi-board">Wifi Development Board</a></li>
+                                <li><a className="dropdown-item" href="/monitor">Activity Monitor Kit</a></li>
+                                <li><a className="dropdown-item" href="/weather">Weather Monitoring Sensor System</a></li>
+                                <li><a className="dropdown-item" href="/module">Solar Charging & Discharging Module</a></li>
+                                <li><a className="dropdown-item" href="/shield">Grove Shield</a></li>
+                                <li><a className="dropdown-item" href="/battery">Solar Panel and Battery</a></li>
+                                <li><a className="dropdown-item" href="/sht40">SHT40 Sensor</a></li>
+                                <li><a className="dropdown-item" href="/stts751">STTS751 Sensor</a></li>
+                                <li><a className="dropdown-item" href="/lis3dh">LIS3DH Sensor</a></li>
+                                <li><a className="dropdown-item" href="/relay">RELAY Sensor</a></li>
+                                <li><a className="dropdown-item" href="/lcd">LCD</a></li>
+                                <li><a className="dropdown-item" href="/buzzer">Buzzer</a></li>
+                                <li><a className="dropdown-item" href="/memory">Memory Unit (W25Q16)</a></li>
+                                <li><a className="dropdown-item" href="/lux">Lux Sensor</a></li>
+                                <li><a className="dropdown-item" href="/uart">UART to USB Converter</a></li>
                             </ul>
                         </li>
                     </ul>
